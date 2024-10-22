3b030995b6b8fe34622ddd9db7dd484b341391fc
JasonRandazza <135364916+JasonRandazza@users.noreply.github.com>
2024-10-21 20:26:52 -0400

    Methods made static

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        Main main = new Main();
         int n = 5;
-        int result = main.factorial(n);
-        int result2 = main.fibonacci(n);
+        int result = factorial(n);
+        int result2 = fibonacci(n);
         System.out.println("Factorial of " + n + " is: " + result);
         System.out.println("Fibonacci of " + n + " is: " + result2);
     }
-    public int factorial (int n) {
+    public static int factorial (int n) {
         if (n <= 1) {
             return 1;
         } else {
             return n * factorial(n - 1);
         }
     }
-    public int fibonacci (int n) {
+    public static int fibonacci (int n) {
         if (n == 1 || n == 2) { //or if (n <=2)
             return 1;           // return 1
         }
